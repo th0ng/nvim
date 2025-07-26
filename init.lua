7325a971bf2b2960250f718c54a2bd23fe00b6d5
Thong Hoang <thonghoangdanh@gmail.com>
2025-07-26 13:32:27 +0300

    Ignore whichkey warnings

--- init.lua
+++ init.lua
@@ -6,6 +6,15 @@ require('bufferline-config')
 require('toggleterm-config')
 require('whichkey')
 
+-- Filter which-key warnings
+local orig_notify = vim.notify
+vim.notify = function(msg, level, opts)
+    if msg:match("which%-key") and level == vim.log.levels.WARN then
+        return
+    end
+    orig_notify(msg, level, opts)
+end
+
 -- Ensure Neovim does not override the terminal's background:
 vim.o.background = "dark"
 vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
